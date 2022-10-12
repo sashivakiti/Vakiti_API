7b5fb2cfe70ab6a753ed01a5ef9f1570d262e451
Sashidhar Vakiti <sashidhar.vakiti@jdplc.com>
2022-10-13 09:41:31 +1100

    Add SuperHero Controller

---
+++ Vakiti_API/Controllers/SuperHeroController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vakiti_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        public SuperHeroController()
+        {
+
+        }
+    }
+}
