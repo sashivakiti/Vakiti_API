8f71ff991f4d520da21d09d143718f37dc51d771
Sashidhar Vakiti <sashidhar.vakiti@jdplc.com>
2022-11-08 12:29:19 +1100

    EF

--- Vakiti_API/Program.cs
+++ Vakiti_API/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataCon>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
